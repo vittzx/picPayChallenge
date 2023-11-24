b9477026aa03f30cb224fdb922277e724d8b288c
Vitor Cosso <vitorcosso@gmail.com>
2023-11-24 12:53:45 -0300

    first commit

---
+++ src/test/java/br/com/challenge/picpay/Desafio/Picpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.picpay.Desafio.Picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
