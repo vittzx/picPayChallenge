95be6143241a7c01cd542567dc554144635fabe5
Vitor Cosso <vitorcosso@gmail.com>
2023-11-25 12:50:30 -0300

    refactor: incluindo mensagens de erro no UserDTO

--- src/main/java/br/com/challenge/picpay/Desafio/Picpay/dto/UserDTO.java
+++ src/main/java/br/com/challenge/picpay/Desafio/Picpay/dto/UserDTO.java
@@ -7,12 +7,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserDTO(@NotBlank String firstName,
-                      @NotBlank String lastName,
-                      @NotBlank @Email String email,
-                      @NotBlank String document,
-                      @NotBlank String password,
-                      @NotNull BigDecimal balance,
-                      @NotNull UserType userType) {
+public record UserDTO(@NotBlank(message = "Primeiro nome é obrigatirio") String firstName,
+                      @NotBlank(message = "Ulitmo nome é obrigatorio") String lastName,
+                      @NotBlank(message = "Email na formatacao tipo email é obrigatorio") @Email String email,
+                      @NotBlank(message = "CPF/CNPJ é obrigatorio") String document,
+                      @NotBlank(message = "SENHA é obrigatorio") String password,
+                      @NotNull(message = "Saldo é obrigatorio") BigDecimal balance,
+                      @NotNull(message = "Tipo do usuario é Obrigatorio: COMMON/MERCHANT") UserType userType) {
     
 }
